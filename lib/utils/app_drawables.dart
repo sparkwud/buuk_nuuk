a0188074a6829c5c27d2397ce96f1631e13f126b
sparkwud <kelvinagbenyo@gmail.com>
2024-06-27 07:21:51 +0000

    add helper fiels

---
+++ lib/utils/app_drawables.dart
@@ -0,0 +1,34 @@
+class AppImages {
+  // Image folder path
+  static const String imagePath = 'assets/images';
+
+  static const String imgProfile = "$imagePath/img_profile.png";
+}
+
+class AppIcons {
+  static const iconsPath = 'assets/icons';
+
+  static const icSearch = '$iconsPath/ic_search.svg';
+
+  // NAV BAR ICONS
+  static const icHome = '$iconsPath/ic_home.svg';
+  static const icHomeFilled = '$iconsPath/ic_home_filled.svg';
+
+  static const icFavorite = '$iconsPath/ic_favorite.svg';
+  static const icFavoriteFilled = '$iconsPath/ic_favorite_filled.svg';
+
+  static const icStore = '$iconsPath/ic_store.svg';
+  static const icStoreFilled = '$iconsPath/ic_store_filled.svg';
+
+  static const icSettings = '$iconsPath/ic_settings.svg';
+  static const icSettingsFilled = '$iconsPath/ic_settings_filled.svg';
+
+  static const icMenu = '$iconsPath/ic_menu.svg';
+}
+
+class AppAnimations {
+  static const animationsPath = "assets/animations";
+
+  static const aniEmptyBox = '$animationsPath/lottie_empty_box.json';
+  static const aniGhost = '$animationsPath/lottie_ghost.json';
+}
