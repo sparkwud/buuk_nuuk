e63d109eec125098c3ca6cd684505fa8fe5ceb46
sparkwud <kelvinagbenyo@gmail.com>
2024-06-28 01:27:05 +0000

    populate category items screen with books from selected category

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -11,42 +11,6 @@ enum Category {
     imageUrl:
         "https://images.unsplash.com/photo-1600181982553-ce7d36051c01?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D",
   ),
-  comic(
-    title: "Comic",
-    query: "comic",
-    imageUrl:
-        "https://images.unsplash.com/photo-1608889476561-6242cfdbf622?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y29taWN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-  ),
-  crime(
-    title: "Crime",
-    query: "crime",
-    imageUrl:
-        "https://images.unsplash.com/photo-1625449281218-cbb6183f0aec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y3JpbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-  ),
-  history(
-    title: "History",
-    query: "history",
-    imageUrl:
-        "https://images.unsplash.com/photo-1613324766451-2d03b2ea8190?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ),
-  horror(
-    title: "Horror",
-    query: "horror",
-    imageUrl:
-        "https://images.unsplash.com/photo-1542691646-b06e145f7a95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9ycm9yJTIwYm9va3N8ZW58MHx8MHx8fDA%3D",
-  ),
-  mystery(
-    title: "Mystery",
-    query: "mystery",
-    imageUrl:
-        "https://images.unsplash.com/photo-1419640303358-44f0d27f48e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
-  ),
-  fantasy(
-    title: "Fantasy",
-    query: "fantasy",
-    imageUrl:
-        "https://images.unsplash.com/photo-1479142506502-19b3a3b7ff33?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8cnhNaGlxdmZpRGd8fGVufDB8fHx8fA%3D%3D",
-  ),
   novel(
     title: "Novel",
     query: "novel",
@@ -59,6 +23,43 @@ enum Category {
     imageUrl:
         // "https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cm9tYW5jZSUyMGJvb2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         "https://images.unsplash.com/photo-1638277294704-507f355ce75e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ),
+  comic(
+    title: "Comic",
+    query: "comic",
+    imageUrl:
+        "https://images.unsplash.com/photo-1608889476561-6242cfdbf622?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y29taWN8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  ),
+
+  history(
+    title: "History",
+    query: "history",
+    imageUrl:
+        "https://images.unsplash.com/photo-1613324766451-2d03b2ea8190?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ),
+  horror(
+    title: "Horror",
+    query: "horror",
+    imageUrl:
+        "https://images.unsplash.com/photo-1542691646-b06e145f7a95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG9ycm9yJTIwYm9va3N8ZW58MHx8MHx8fDA%3D",
+  ),
+  crime(
+    title: "Crime",
+    query: "crime",
+    imageUrl:
+        "https://images.unsplash.com/photo-1625449281218-cbb6183f0aec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y3JpbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  ),
+  mystery(
+    title: "Mystery",
+    query: "mystery",
+    imageUrl:
+        "https://images.unsplash.com/photo-1419640303358-44f0d27f48e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D",
+  ),
+  fantasy(
+    title: "Fantasy",
+    query: "fantasy",
+    imageUrl:
+        "https://images.unsplash.com/photo-1479142506502-19b3a3b7ff33?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8cnhNaGlxdmZpRGd8fGVufDB8fHx8fA%3D%3D",
   );
 
   const Category({
---
+++ lib/views/screens/category_items_screen.dart
@@ -0,0 +1,187 @@
+import 'dart:math' as math;
+
+import 'package:buuk_nuuk/models/category_model.dart';
+import 'package:buuk_nuuk/providers/book_service_providers.dart';
+import 'package:buuk_nuuk/utils/pallete.dart';
+import 'package:buuk_nuuk/utils/text_theme.dart';
+import 'package:buuk_nuuk/utils/widget_extensions.dart';
+import 'package:buuk_nuuk/views/screens/book_details_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategoryItemScreen extends ConsumerStatefulWidget {
+  const CategoryItemScreen({
+    super.key,
+    required this.category,
+  });
+  final Category category;
+
+  @override
+  ConsumerState<CategoryItemScreen> createState() => _CategoryItemScreenState();
+}
+
+class _CategoryItemScreenState extends ConsumerState<CategoryItemScreen> {
+  final _random = math.Random();
+  late Parameters params;
+
+  @override
+  void initState() {
+    super.initState();
+    params = Parameters(category: widget.category, maxResult: 20);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categoryItems = ref.watch(
+      booksFromCategoryProvider(params),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: appColors.black),
+        title: Text(
+          widget.category.name.toTitleCase(),
+          style: textTheme.displayMedium,
+        ),
+      ),
+      body: categoryItems.when(
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (error, stacktrace) {
+          return const Center(
+            child: Text("Opps! Try again later!"),
+          );
+        },
+        data: (books) => GridView.builder(
+          itemCount: books.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //childAspectRatio: 16 / 15,
+            crossAxisCount: 2,
+            mainAxisExtent: 260,
+            crossAxisSpacing: 0,
+            mainAxisSpacing: 0,
+          ),
+          itemBuilder: (context, index) {
+            final book = books[index];
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BookDetailsScreen(
+                        bookDetails: book,
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: constraints.maxHeight / 2,
+                            child: Stack(
+                              alignment: Alignment.topCenter,
+                              children: [
+                                Container(
+                                  height: constraints.maxHeight / 2.5,
+                                  decoration: BoxDecoration(
+                                    color:
+                                        appColors.boxColors[_random.nextInt(7)],
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 0,
+                                  child: Card(
+                                    margin: EdgeInsets.zero,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: SizedBox(
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: Image(
+                                          height: constraints.maxHeight / 2,
+                                          width: constraints.maxWidth / 2,
+                                          image: NetworkImage(
+                                            "${book.imageLinks!.thumbnail}",
+                                          ),
+                                          fit: BoxFit.fill, // use this
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  book.title,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineMedium
+                                      ?.copyWith(
+                                        fontSize: constraints.maxWidth * 0.09,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                                Text(
+                                  "${book.authors?.join(', ')}",
+                                  maxLines: 1,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineMedium
+                                      ?.copyWith(
+                                        fontSize: constraints.maxWidth * 0.09,
+                                      ),
+                                ),
+                                Container(
+                                  height: constraints.maxHeight * 0.13,
+                                  width: constraints.maxWidth * 0.35,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: appColors.black,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Text(
+                                    "\$${book.pageCount}",
+                                    style: TextStyle(
+                                      fontSize: constraints.maxWidth * 0.08,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
