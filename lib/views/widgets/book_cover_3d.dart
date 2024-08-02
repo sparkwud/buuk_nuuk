f64b06db2963658b2b77880fe18a64403b640332
sparkwud <kelvinagbenyo@gmail.com>
2024-08-02 23:59:44 +0000

    3d book cover redesign

--- lib/views/widgets/book_cover_3d.dart
+++ lib/views/widgets/book_cover_3d.dart
@@ -1,11 +1,19 @@
+import 'package:buuk_nuuk/utils/app_drawables.dart';
+import 'package:buuk_nuuk/utils/context_extension.dart';
+import 'package:buuk_nuuk/utils/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BookCover3D extends StatefulWidget {
-  final String imageUrl;
+  final String? imageUrl;
+  final String? title;
+  final String? author;
 
   const BookCover3D({
     super.key,
-    required this.imageUrl,
+    this.imageUrl,
+    this.title,
+    this.author,
   });
 
   @override
@@ -16,15 +24,18 @@ class _BookCover3DState extends State<BookCover3D> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100,
-      height: 200,
+      // width: 100,
+      // height: 200,
+      width: context.width * 0.3,
+      // height: context.width * 0.4,
       child: Transform(
         transform: Matrix4.identity()
           ..setEntry(2, 2, 0.001)
           ..rotateY(0.25),
         alignment: FractionalOffset.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             AspectRatio(
               aspectRatio: 2 / 3,
@@ -40,13 +51,33 @@ class _BookCover3DState extends State<BookCover3D> {
                   ],
                 ),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
+                  borderRadius: BorderRadius.circular(10.0),
                   child: Image.network(
-                    widget.imageUrl,
+                    widget.imageUrl ?? AppImages.errorLink,
                     fit: BoxFit.fitWidth,
                   ),
                 ),
               ),
+            ),
+            const Gap(12),
+            Text(
+              widget.title ?? "title...",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: colorScheme.secondary,
+                    fontSize: context.width * 0.035,
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+            Text(
+              widget.author ?? "authors...",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    // color: colorScheme.secondary,
+                    fontSize: context.width * 0.03,
+                  ),
             ),
           ],
         ),
