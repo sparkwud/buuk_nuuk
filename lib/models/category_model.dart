80d96a88215c50d19307de0941bd701738b20c8e
kisa <kelvinagbenyo@gmail.com>
2024-08-05 04:21:00 +0000

    update book category enum

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -21,7 +21,7 @@ enum Category {
     title: "Coding",
     query: "Coding",
     imageUrl:
-        "https://images.unsplash.com/photo-1672309558498-cfcc89afff25?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvZGluZyUyMGJvb2tzfGVufDB8fDB8fHww",
+        "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ),
   romance(
     title: "Romance",
