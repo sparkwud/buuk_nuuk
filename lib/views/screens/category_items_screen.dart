fa59037aa678c556f712f86dd7ffbd51162ec93f
kisa <kelvinagbenyo@gmail.com>
2024-08-09 12:37:00 +0000

    update category items ui

--- lib/views/screens/category_items_screen.dart
+++ lib/views/screens/category_items_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:math' as math;
-
+import 'package:buuk_nuuk/models/book_model.dart';
 import 'package:buuk_nuuk/models/category_model.dart';
 import 'package:buuk_nuuk/providers/book_service_providers.dart';
 import 'package:buuk_nuuk/utils/pallete.dart';
@@ -21,7 +20,6 @@ class CategoryItemScreen extends ConsumerStatefulWidget {
 }
 
 class _CategoryItemScreenState extends ConsumerState<CategoryItemScreen> {
-  final _random = math.Random();
   late Parameters params;
 
   @override
@@ -32,155 +30,126 @@ class _CategoryItemScreenState extends ConsumerState<CategoryItemScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final categoryItems = ref.watch(
-      booksFromCategoryProvider(params),
-    );
+    final categoryItems = ref.watch(booksFromCategoryProvider(params));
     return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        title: Text(
-          widget.category.name.toTitleCase(),
-          style: textTheme.displayMedium!.copyWith(color: colorScheme.primary),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              // title: Text(
+              //   widget.category.name.toTitleCase(),
+              //   style: textTheme.headlineMedium!.copyWith(color: Colors.white),
+              // ),
+              title: Text(
+                widget.category.name.toTitleCase(),
+                style: textTheme.displayMedium!.copyWith(
+                  color: colorScheme.primary,
+                ),
+              ),
+              background: Image.network(
+                widget.category.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: categoryItems.when(
+              loading: () => const SliverFillRemaining(
+                child: Center(child: CircularProgressIndicator()),
+              ),
+              error: (error, stacktrace) => const SliverFillRemaining(
+                child: Center(child: Text("Oops! Try again later!")),
+              ),
+              data: (books) => SliverGrid(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.7,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => BookCard(book: books[index]),
+                  childCount: books.length,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BookCard extends StatelessWidget {
+  final Book book;
+
+  const BookCard({super.key, required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => BookDetailsScreen(bookDetails: book),
         ),
       ),
-      body: categoryItems.when(
-        loading: () => const Center(
-          child: CircularProgressIndicator(),
-        ),
-        error: (error, stacktrace) {
-          return const Center(
-            child: Text("Opps! Try again later!"),
-          );
-        },
-        data: (books) => GridView.builder(
-          itemCount: books.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            //childAspectRatio: 16 / 15,
-            crossAxisCount: 2,
-            mainAxisExtent: 260,
-            crossAxisSpacing: 0,
-            mainAxisSpacing: 0,
-          ),
-          itemBuilder: (context, index) {
-            final book = books[index];
-            return Padding(
-              padding: const EdgeInsets.all(16),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BookDetailsScreen(
-                        bookDetails: book,
-                      ),
-                    ),
-                  );
-                },
-                child: Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: LayoutBuilder(
-                    builder: (context, constraints) {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: constraints.maxHeight / 2,
-                            child: Stack(
-                              alignment: Alignment.topCenter,
-                              children: [
-                                Container(
-                                  height: constraints.maxHeight / 2.5,
-                                  decoration: BoxDecoration(
-                                    color:
-                                        appColors.boxColors[_random.nextInt(7)],
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 0,
-                                  child: Card(
-                                    margin: EdgeInsets.zero,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: SizedBox(
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(12),
-                                        child: Hero(
-                                          tag: book,
-                                          child: Image(
-                                            height: constraints.maxHeight / 2,
-                                            width: constraints.maxWidth / 2,
-                                            image: NetworkImage(
-                                              book.getImageUrl,
-                                            ),
-                                            fit: BoxFit.fill, // use this
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  book.title,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headlineMedium
-                                      ?.copyWith(
-                                        fontSize: constraints.maxWidth * 0.09,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                                Text(
-                                  "${book.authors?.join(', ')}",
-                                  maxLines: 1,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headlineMedium
-                                      ?.copyWith(
-                                        fontSize: constraints.maxWidth * 0.09,
-                                      ),
-                                ),
-                                Container(
-                                  height: constraints.maxHeight * 0.13,
-                                  width: constraints.maxWidth * 0.35,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                    color: appColors.black,
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                  child: Text(
-                                    "\$${book.pageCount}",
-                                    style: TextStyle(
-                                      fontSize: constraints.maxWidth * 0.08,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      );
-                    },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 4,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(12),
+                ),
+                child: Hero(
+                  tag: book,
+                  child: Image.network(
+                    book.getImageUrl,
+                    fit: BoxFit.cover,
+                    width: double.maxFinite,
                   ),
                 ),
               ),
-            );
-          },
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      book.title,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                    Expanded(
+                      child: Text(
+                        book.getAuthors,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
